6c7774ea44cdcabb08a9c1734bffbd48614a798a
K-30\TEACHER <66kbz@soscandia.org>
2024-12-03 10:55:53 +0700

    Реализовал интерфейсы животных, создал их классы

---
+++ FermMad/FermMad/IAnimal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FermMad
+{
+    public interface IAnimal
+    {
+        bool Health { get;  }
+        int Eat { get;  }
+        int PriceBuy { get;  }
+        int PriceSell { get;  }
+    }
+    
+}
